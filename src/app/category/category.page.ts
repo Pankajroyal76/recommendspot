72d96fa39bde4a460d0534728e791bf6ebdaab9f
ajesh1234 <indiitexperts@gmail.com>
2021-02-02 11:02:49 +0530

    chnages

--- src/app/category/category.page.ts
+++ src/app/category/category.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiserviceService } from '../services/apiservice.service';
+import { GlobalFooService } from '../services/globalFooService.service';
+import { config } from '../services/config';
+import { Router } from '@angular/router';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-category',
@@ -6,11 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./category.page.scss'],
 })
 export class CategoryPage implements OnInit {
-categorytab: string = "category";
+	categorytab: string = "category";
   	isAndroid: boolean = false;
-  constructor() { }
+  	categories: any;
+  	
+  	constructor(public apiService: ApiserviceService, public router: Router, public location: Location, private globalFooService: GlobalFooService){ }
 
-  ngOnInit() {
-  }
+  	ngOnInit() {
+
+  		
+  	}
+
+  	ionViewDidEnter() {
+
+  		this.getCategories();
+  	}
+
+  	getCategories(){
+
+  		this.apiService.presentLoading();
+  		var dict = {
+	    	user_id: localStorage.getItem('userId')
+	    }
+	  
+	    this.apiService.postData(dict,'categories').subscribe((result) => { 
+	     this.apiService.stopLoading();  
+	     console.log(result.data)
+	      if(result.status == 1){
+	        this.categories = result.data;	        
+	       // this.getData();
+	      }
+	      else{
+	        this.apiService.presentToast('Error while sending request,Please try after some time','success');
+	      }
+	    },
+	    err => {
+	        this.apiService.presentToast('Technical error,Please try after some time','success');
+	    });
+  	}
 
 }
